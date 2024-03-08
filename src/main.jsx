0dc9a977ff3620dbe8e458d586b025f79bdedeed
nikthewik <nikthewik@gmail.com>
2024-03-08 23:56:42 +0100

    Add media queries. Add ScreenWakeLock.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import ScreenWakeLock from "screen-wake-lock/lib/ScreenWakeLock";
 import "./index.css";
 import App from "./components/App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <ScreenWakeLock />
     <App />
   </React.StrictMode>
 );
